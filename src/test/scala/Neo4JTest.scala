c9f062d91e54c92271fac53457e1ec4c0e59f42c
Sébastien Willmann <sebastien.willmann@gmail.com>
2016-11-16 22:20:39 +0100

    Test database

--- src/test/scala/Neo4JTest.scala
+++ src/test/scala/Neo4JTest.scala
@@ -1,12 +1,13 @@
 package main
 
 import java.io.File
+import java.nio.file.Files
 
 import org.scalatest._
 import org.neo4j.graphdb._
 import org.neo4j.graphdb.factory._
 
-class StackSpecTest extends UnitSpec {
+class Neo4JTest extends UnitSpec {
 
   /*"A Stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
@@ -23,8 +24,30 @@ class StackSpecTest extends UnitSpec {
     }
   }*/
 
+  abstract class RelTypes extends RelationshipType
+  case object Knows extends RelTypes {
+    val name = "KNOWS"
+  }
+
   "A database" should "store and retrieve values" in {
-    val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File("/tmp/testdb"))
+    val dbFile = Files.createTempDirectory("testdb").toFile()
+    val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(dbFile)
+
+    val writeTx = graphDb.beginTx()
+    val firstNode = graphDb.createNode()
+    firstNode.setProperty( "message", "Hello, " )
+    val secondNode = graphDb.createNode()
+    secondNode.setProperty( "message", "World!" )
+
+    val relationship = firstNode.createRelationshipTo( secondNode, Knows)
+    relationship.setProperty( "message", "brave Neo4j " )
+
+    assert(firstNode.getProperty( "message" ) === "Hello, ")
+    assert(relationship.getProperty( "message" ) === "brave Neo4j ")
+    assert(secondNode.getProperty( "message" ) === "World!")
+
+    writeTx.success()
+
     graphDb.shutdown()
   }
 }
